bd49b49b26e586bd6889206ab8c55a9c9716ac34
gill_pevzner <grigoripevzner@gmail.com>
2022-01-31 11:18:57 +0200

    M1-added_index_page_and_connected_throughout_the_app

---
+++ pages/views.py
@@ -0,0 +1,10 @@
+import imp
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('<h1>Hello World</h1>')
+
+# Create your views here.
